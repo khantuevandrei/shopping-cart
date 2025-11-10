b6f54de5d1a4a804a99486a86f584599fc2817b7
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-10 12:46:54 +0500

    Add css module for Home, add img to assets

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,17 @@
+import styles from "./Home.module.css";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className={styles.container}>
+      <img src="src/assets/shop.jpg" alt="" />
+      <div>
+        <h1>Welcome to Shop!</h1>
+        <p>Use the navigation bar on top of the page.</p>
+        <p>Visit us at:</p>
+        <p>123, Fake Road, Fake Town</p>
+        <p>Contact us:</p>
+        <p>fake@fakemail.com</p>
+      </div>
+    </div>
+  );
 }
