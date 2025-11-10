3e16977dfdba870ea3a275aeedd2cce0511f1ef3
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-10 14:13:48 +0500

    Add loading screen while fetching for items

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -3,16 +3,27 @@ import styles from "./Shop.module.css";
 
 export default function Shop() {
   const [itemList, setItemList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchItems() {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      setItemList(data);
+      try {
+        setIsLoading(true);
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok) throw new Error("Failed to fetch items");
+        const data = await response.json();
+        setItemList(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     fetchItems();
   }, []);
+
+  if (isLoading) return <p className={styles.loading}>Loading...</p>;
 
   return (
     <div className={styles.cardGrid}>
@@ -20,7 +31,6 @@ export default function Shop() {
         return (
           <div key={item.id} className={styles.card}>
             <h2 className={styles.itemTitle}>{item.title}</h2>
-
             <img className={styles.img} src={item.image} alt={item.title} />
             <h2 className={styles.itemPrice}>${item.price}</h2>
             <div>
