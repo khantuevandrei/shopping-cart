d85801df2ba05e4b50504e408729f98e33835dd2
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-10 14:02:05 +0500

    Add fetch for items to Shop & css module with styling

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,14 +3,16 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img src="src/assets/shop.jpg" alt="" />
+      <img className={styles.img} src="src/assets/shop.jpg" alt="" />
       <div>
-        <h1>Welcome to Shop!</h1>
-        <p>Use the navigation bar on top of the page.</p>
-        <p>Visit us at:</p>
-        <p>123, Fake Road, Fake Town</p>
-        <p>Contact us:</p>
-        <p>fake@fakemail.com</p>
+        <h1 className={styles.heading}>Welcome to Shop!</h1>
+        <p className={styles.para}>
+          Use the navigation bar on top of the page.
+        </p>
+        <p className={styles.para}>Visit us at:</p>
+        <p className={styles.para}>123, Fake Road, Fake Town</p>
+        <p className={styles.para}>Contact us:</p>
+        <p className={styles.para}>fake@fakemail.com</p>
       </div>
     </div>
   );
--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react";
+import styles from "./Shop.module.css";
+
 export default function Shop() {
-  return <h1>Shop</h1>;
+  const [itemList, setItemList] = useState([]);
+
+  useEffect(() => {
+    async function fetchItems() {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setItemList(data);
+    }
+
+    fetchItems();
+  }, []);
+
+  return (
+    <div className={styles.cardGrid}>
+      {itemList.map((item) => {
+        return (
+          <div key={item.id} className={styles.card}>
+            <h2 className={styles.itemTitle}>{item.title}</h2>
+
+            <img className={styles.img} src={item.image} alt={item.title} />
+            <h2 className={styles.itemPrice}>${item.price}</h2>
+            <div>
+              <button>-</button>
+              <input className={styles.quantity} type="num" value={0} />
+              <button>+</button>
+            </div>
+            <button className={styles.buy}>Buy</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
