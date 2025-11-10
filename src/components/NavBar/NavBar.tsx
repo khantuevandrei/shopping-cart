2f7cf83785cfa8530169dd45fb39fd2301c129fe
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-10 12:25:58 +0500

    Add css module for NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   return (
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/shop">Shop</Link>
-      <Link to="/cart">Cart</Link>
+    <header className={styles.navBar}>
+      <Link to="/" className={styles.link}>
+        Home
+      </Link>
+      <Link to="/shop" className={styles.link}>
+        Shop
+      </Link>
+      <Link to="/cart" className={styles.link}>
+        Cart
+      </Link>
     </header>
   );
 }
