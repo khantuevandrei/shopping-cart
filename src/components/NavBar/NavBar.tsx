5978a20f8b938761fa03e05c5d95a46191036a67
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-12 10:32:14 +0500

    Style NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,14 +8,27 @@ type NavBarProps = {
 export default function NavBar({ cartAmount }: NavBarProps) {
   return (
     <header className={styles.navBar}>
-      <Link to="/" className={styles.link}>
-        Home
+      <Link to="/" className={styles.logo}>
+        <div className={styles.logo}>
+          <h1 className={styles.heading}>Tote</h1>
+          <p className={styles.para}>Designer clothes & accessories</p>
+        </div>
       </Link>
-      <Link to="/shop" className={styles.link}>
-        Shop
-      </Link>
-      <Link to="/cart" className={styles.link}>
-        Cart: {cartAmount}
+      <div className={styles.middle}>
+        <Link to="/" className={styles.link}>
+          About
+        </Link>
+        <Link to="/shop" className={styles.link}>
+          Shop
+        </Link>
+      </div>
+      <Link to="/cart" className={styles.cart}>
+        <img
+          className={styles.icon}
+          src="src/assets/shopping-cart.png"
+          alt="shopping cart"
+        />
+        <div className={styles.amount}>{cartAmount}</div>
       </Link>
     </header>
   );
