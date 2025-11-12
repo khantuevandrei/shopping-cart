f63f184c69d182f6739c7fcf79dfc48e9b61d3f2
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-12 11:03:20 +0500

    Style Home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,16 +3,14 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img className={styles.img} src="src/assets/shop.jpg" alt="" />
       <div>
-        <h1 className={styles.heading}>Welcome to Shop!</h1>
+        <h1 className={styles.heading}>Welcome to Tote</h1>
         <p className={styles.para}>
-          Use the navigation bar on top of the page.
+          100 year history of handmade jewelry & clothing.
         </p>
-        <p className={styles.para}>Visit us at:</p>
-        <p className={styles.para}>123, Fake Road, Fake Town</p>
-        <p className={styles.para}>Contact us:</p>
-        <p className={styles.para}>fake@fakemail.com</p>
+        <p className={styles.para}>
+          We take pride in the quality of our goods.
+        </p>
       </div>
     </div>
   );
